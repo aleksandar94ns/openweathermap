43b9b6c6d30bd6e0663a7dce56cb2a382ef84284
EXECOM\aradosevic <araodsevic@execom.eu>
2021-01-25 18:06:05 +0100

    Initial project setup

---
+++ src/main/java/com/aradosevic/openweathermap/openweathermap/dto/CityWeatherDto.java
@@ -0,0 +1,23 @@
+package com.aradosevic.openweathermap.openweathermap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@Builder
+public class CityWeatherDto {
+
+  private Integer id;
+
+  private String name;
+}
